d8d959207317b18d9e0b4937503b816ab842af6b
MerryPopup <dennis.vizins@futuregames.se>
2024-01-27 12:31:05 +0100

    Added sprite move direction

--- AnimationHandler.gd
+++ AnimationHandler.gd
@@ -1,12 +1,29 @@
 extends Node3D
 @onready var _animated_sprite = $Body/DeerBody
-
+@onready var _sprite_direction = $Body/DeerBody/SpriteDirection
+@onready var isLeft = true
 @export var character_node: Node3D
+
 
 func update_velocity(velocity):
 	if velocity.length() > 0.1:
 		_animated_sprite.play("default")
-	
+		if velocity.x < -0.1 :
+			if isLeft:
+				pass
+			else :
+				_sprite_direction.play("MoveLeft")
+				isLeft = true
+			
+			
+		else:
+			if !isLeft:
+				pass
+			else :
+				_sprite_direction.play("MoveRight")
+				isLeft = false
+			
+			
 	else:
 		_animated_sprite.play("Idle")
 
