63ec77a627929255db356630f57cd645fa097f5e
Tristan Louet <tristan.louet.dev@gmail.com>
2024-01-27 20:39:15 +0100

    citizen and maths

--- scripts/characters/citizen.gd
+++ scripts/characters/citizen.gd
@@ -1,10 +1,27 @@
 extends CharacterBody3D
 
+class Target:
+	enum Mode { OBJECT, POSITION }
+	var mode: Mode
+	var target: Variant
+	
+	func _init(target_or_position: Variant):
+		assert(target_or_position is Node3D or target_or_position is Vector3)
+		target = target_or_position
+		mode = Mode.OBJECT if target is Node3D else Mode.POSITION
+	
+	func get_position():
+		if mode == Mode.OBJECT:
+			return (target as Node3D).global_position
+		return target as Vector3
+
+var _target: Target = null
+
 var _speed = 1.0
+@onready var timer: Timer = $Timer
 @onready var navigation: NavigationAgent3D = $NavigationAgent3D
 var stage: citizens_info.Stage = citizens_info.Stage.DULL
 var job: citizens_info.Job = citizens_info.Job.LIVE_DULL_LIFE
-var _target: Node3D = null
 
 signal character_moved(velocity)
 signal stage_changed(stage: citizens_info.Stage)
@@ -24,20 +41,20 @@ var state_machine = {
 			# a target position within 50 meters to slowly go to, and then wait a
 			# random amount of time before doing it again.
 			_speed = 1.0
-			,
+			_roam(),
 		JobState.EXIT: func():
-			pass,
+			_target = null,
 	},
 	citizens_info.Job.PRAY: {
 		JobState.ENTER: func():
 			_speed = 3.0
 			var ship = world_info.ship
 			assert(ship.has_free_praying_spot())
-			_target = ship.reserve_praying_spot(),
+			_target = Target.new(ship.reserve_praying_spot()),
 		JobState.PROCESS: func(delta):
 			pass,
 		JobState.EXIT: func():
-			pass,
+			_target = null,
 	},
 	citizens_info.Job.DEFEND: {
 		JobState.ENTER: func():
@@ -48,6 +65,12 @@ var state_machine = {
 			pass,
 	}
 }
+
+func _roam():
+	# this is still biased to closer positions, but I can't be bothered right now
+	var random_direction = maths.random_inside_unit_circle()
+	var random_distance = randf_range(5.0, 50.0)
+	_target = Target.new(Vector3(random_direction.x * random_distance, global_position.y, random_direction.y * random_distance))
 
 func _get_job_func(state: JobState):
 	return state_machine[job][state]
@@ -67,7 +90,7 @@ func _physics_process(_delta):
 	velocity.x = move_toward(velocity.x, 0, _speed)
 	velocity.z = move_toward(velocity.z, 0, _speed)
 	if _target != null:
-		navigation.target_position = _target.global_position
+		navigation.target_position = _target.get_position()
 		await get_tree().physics_frame
 		
 		var direction = (navigation.get_next_path_position() - global_position).normalized()
