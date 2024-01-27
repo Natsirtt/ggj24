d098bd8c3c3b066f0d4d7f52346ffae9f3c4b165
MerryPopup <dennis.vizins@futuregames.se>
2024-01-28 00:26:12 +0100

    Added townie skin and anims plus cool stuff

--- AnimationHandler.gd
+++ AnimationHandler.gd
@@ -4,6 +4,8 @@ class_name AnimationHandler extends Node3D
 @onready var _sprite_direction = $Body/DeerBody/SpriteDirection
 var isLeft = true
 var is_interacting = false
+var timer : Timer
+@export var skin : String = "Townie"
 @export var character_node: Node3D
 
 
@@ -28,29 +30,37 @@ func update_velocity(velocity):
 		return
 		
 	if velocity.length() > 0.1:
-		_animated_sprite.play("Player_Walk")
+		_animated_sprite.play(skin + "_Walk")
 
 	else:
-		_animated_sprite.play("Player_Idle")
+		_animated_sprite.play(skin + "_Idle")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	character_node.connect("character_moved", update_velocity)
-
-
+	character_node.connect("character_stopped", update_velocity.bind(Vector3.ZERO))
+	character_node.connect("character_interacted", _handleInteract.bind(null))
+	timer = Timer.new()
+	add_child(timer)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("interact"):
-		_playAnimWithInto("Player_Joke")
-		self.is_interacting = true
-	elif Input.is_action_just_released("interact"): 
-		self.is_interacting = false
-		
+	pass
 	
-
+	
+func _handleInteract(context):
+	_playAnimWithInto("Player_Joke")
+	self.is_interacting = true
+	
+	print("AnimHandleInteract")
+	
+	timer.one_shot = true
+	timer.timeout.connect(func(): is_interacting = false, CONNECT_ONE_SHOT)
+	timer.start(5)
+	
+	
 func _playAnimWithInto(AnimName):
 	_animated_sprite.play(AnimName + "_Into")
 	_animated_sprite.animation_finished.connect(func():	_animated_sprite.play(AnimName + "_Idle"), CONNECT_ONE_SHOT)
 	
 	
-	pass
+	
