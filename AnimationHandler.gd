16b7eddccc7f72c7bc89f8ef64e33907e3536d8d
MerryPopup <dennis.vizins@futuregames.se>
2024-01-27 11:42:45 +0100

    Changed for a signal based architecture

--- AnimationHandler.gd
+++ AnimationHandler.gd
@@ -1,6 +1,8 @@
 extends Node3D
 @onready var _animated_sprite = $Body/DeerBody
 
+@export var character_node: Node3D
+
 func update_velocity(velocity):
 	if velocity.length() > 0.1:
 		_animated_sprite.play("default")
@@ -10,8 +12,7 @@ func update_velocity(velocity):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	pass # Replace with function body.
+	character_node.connect("character_moved", update_velocity)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- player.gd
+++ player.gd
@@ -2,9 +2,9 @@ extends CharacterBody3D
 
 @onready var animation_handler = $Animation
 
+signal character_moved(velocity)
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
 
 func _physics_process(_delta):
 	# Get the input direction and handle the movement/deceleration.
@@ -12,16 +12,13 @@ func _physics_process(_delta):
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
-	animation_handler.update_velocity(direction)
-	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
-		
-	
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+	character_moved.emit(velocity)
 
 
 	move_and_slide()
