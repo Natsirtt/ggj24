7cc4b74d88d866a544854de534a1090b459deccb
Tristan Louet <tristan.louet.dev@gmail.com>
2024-01-27 21:14:38 +0100

    citizens roaming

--- scripts/characters/citizen.gd
+++ scripts/characters/citizen.gd
@@ -1,5 +1,10 @@
 extends CharacterBody3D
 
+@export var min_dull_life_idle_time = 5.0
+@export var max_dull_life_ilde_time = 20.0
+@export var min_dull_life_roam_distance = 5.0
+@export var max_dull_life_roam_distance = 15.0
+
 class Target:
 	enum Mode { OBJECT, POSITION }
 	var mode: Mode
@@ -24,6 +29,7 @@ var stage: citizens_info.Stage = citizens_info.Stage.DULL
 var job: citizens_info.Job = citizens_info.Job.LIVE_DULL_LIFE
 
 signal character_moved(velocity)
+signal character_stopped
 signal stage_changed(stage: citizens_info.Stage)
 
 enum JobState {
@@ -35,14 +41,21 @@ enum JobState {
 var state_machine = {
 	citizens_info.Job.LIVE_DULL_LIFE: {
 		JobState.ENTER: func():
-			pass,
-		JobState.PROCESS: func(delta):
 			# In dull life mode, citizens roam aimlessly. So we make them pick
 			# a target position within 50 meters to slowly go to, and then wait a
 			# random amount of time before doing it again.
 			_speed = 1.0
-			_roam(),
+			timer.process_callback = Timer.TIMER_PROCESS_IDLE
+			timer.start(randf_range(min_dull_life_idle_time, max_dull_life_ilde_time))
+			timer.timeout.connect(func():
+				_roam()
+				timer.wait_time = randf_range(min_dull_life_idle_time, max_dull_life_ilde_time)
+			),
+		JobState.PROCESS: func(delta):
+			pass,
 		JobState.EXIT: func():
+			timer.timeout.disconnect(_roam)
+			timer.stop()
 			_target = null,
 	},
 	citizens_info.Job.PRAY: {
@@ -69,7 +82,7 @@ var state_machine = {
 func _roam():
 	# this is still biased to closer positions, but I can't be bothered right now
 	var random_direction = maths.random_inside_unit_circle()
-	var random_distance = randf_range(5.0, 50.0)
+	var random_distance = randf_range(min_dull_life_roam_distance, max_dull_life_roam_distance)
 	_target = Target.new(Vector3(random_direction.x * random_distance, global_position.y, random_direction.y * random_distance))
 
 func _get_job_func(state: JobState):
@@ -87,6 +100,8 @@ func _process(delta):
 	_get_job_func(JobState.PROCESS).call(delta)
 
 func _physics_process(_delta):
+	var was_stopped = velocity.is_zero_approx()
+	var is_stopped = true
 	velocity.x = move_toward(velocity.x, 0, _speed)
 	velocity.z = move_toward(velocity.z, 0, _speed)
 	if _target != null:
@@ -97,6 +112,9 @@ func _physics_process(_delta):
 		if direction.length() > 0.01 and not navigation.is_target_reached():
 			velocity.x = direction.x * _speed
 			velocity.z = direction.z * _speed
+			is_stopped = false
 			character_moved.emit(velocity)
+	if not was_stopped and is_stopped:
+		character_stopped.emit()
 		
 	move_and_slide()
