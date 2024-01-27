23626030d1127c20554ac34cc57f4a6f3d9478e4
Tristan Louet <tristan.louet.dev@gmail.com>
2024-01-27 17:56:05 +0100

    Added a character_moved signal to Citizens

--- scripts/characters/citizen.gd
+++ scripts/characters/citizen.gd
@@ -1,15 +1,16 @@
 extends CharacterBody3D
 
-const SPEED = 4.0
+const SPEED = 3.0
 @onready var navigation: NavigationAgent3D = $NavigationAgent3D
+
+signal character_moved(velocity)
 
 func _ready():
 	pass
 
 func _physics_process(_delta):
-	if navigation.target_position.is_zero_approx():
-		navigation.target_position = player_info.player.global_position
-		await get_tree().physics_frame
+	navigation.target_position = player_info.player.global_position
+	await get_tree().physics_frame
 		
 	var direction = (navigation.get_next_path_position() - global_position).normalized()
 	if direction.length() > 0.01 and not navigation.is_target_reached():
@@ -18,5 +19,5 @@ func _physics_process(_delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	character_moved.emit(velocity)
 	move_and_slide()
